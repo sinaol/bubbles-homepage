e572cf4bf970fc85366a9edb2739a1a2d7b40413
sina olfaty <sinadevelope@gmail.com>
2021-03-14 14:12:23 +0330

    committing all

---
+++ script.js
@@ -0,0 +1,13 @@
+var image = document.getElementById("image");
+var inner = document.getElementById("innerCircle");
+var body = document.getElementById("body");
+
+function change1() {
+    image.style.filter = "blur(1px)";
+
+
+}
+
+function change2() {
+    image.style.filter = "blur(0px)";
+}
